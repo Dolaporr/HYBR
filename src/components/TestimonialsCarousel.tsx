b24bc5e59fa0e0ddc6e570b430e92b536b004424
Dolaporr <Dolaporr@users.noreply.github.com>
2026-07-06 20:47:05 +0100

    Revert "Fix Peter testimonial avatar frame"
    This reverts commit fe0f79b1a4e2668a6bee668f931e055fd9dba2a6.

--- src/components/TestimonialsCarousel.tsx
+++ src/components/TestimonialsCarousel.tsx
@@ -93,16 +93,12 @@ function TestimonialCard({
     >
       <div className="home-testimonial-avatar relative flex size-[116px] items-center justify-center rounded-full">
         <span className="absolute inset-0 rounded-full border-[1.5px] border-[#8dc540] opacity-60" />
-        <span
-          className={`home-testimonial-avatar-image size-[88px] overflow-hidden rounded-full border-[3px] border-[#8dc540] ${testimonial.avatarClassName ?? ""}`}
-        >
-          <img
-            alt={ariaHidden ? "" : testimonial.name}
-            className="home-testimonial-avatar-photo size-full object-cover"
-            draggable={false}
-            src={testimonial.image}
-          />
-        </span>
+        <img
+          alt={ariaHidden ? "" : testimonial.name}
+          className={`home-testimonial-avatar-image size-[88px] rounded-full border-[3px] border-[#8dc540] object-cover ${testimonial.avatarClassName ?? ""}`}
+          draggable={false}
+          src={testimonial.image}
+        />
       </div>
       <div>
         <blockquote className="home-testimonial-quote">
